53ff498aa91bdf858d50aeab1a69fb43541897c5
Syed Rameez <rameezsyed94@gmail.cm>
2024-09-09 05:25:05 +0000

    second commit

---
+++ check.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#constant varibale
+
+#readonly college="PES Engineering College"
+
+#echo "my college name is ${college}"
+
+# read option
+#
+read -p "please enter name: " student
+read -p "please enter subject name:" subject
+
+echo " my name is ${student} i am doing ${subject} course for bright future"
---
+++ index.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#constant varibale
+
+#readonly college="PES Engineering College"
+
+#echo "my college name is ${college}"
+
+# read option
+#
+#read -p "please enter name: " student
+#read -p "please enter subject name:" subject
+
+#echo " my name is ${student} i am doing ${subject} course for bright future"
+
+
+#student=$(1)
+#course=$(2)
+#password=$(3)
+
+#echo
+
+#echo "my name is ${student} i am doing ${course} and my password is ${password}"
+
+#echo
+
+
+
+student=$1
+course=$2
+password=$3
+
+
+echo "my name is ${student}, I am doing ${course}, and my password is ${password}"
+
