98f6c49a03a851efe37338540d8dcecc18d774a1
Your Name <you@example.com>
2025-01-27 08:19:10 +0000

    3

---
+++ shell-g/var2.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#varibale name type
+#1
+_varaiblename=syed
+
+echo "my name is ${_varaiblename}"
+#2
+_varaible1name=Rameez
+
+echo "my name is ${_varaible1name}"
+
+#3
+NAME=Syed
+name=noorul
+NaMe=rameez
+
+echo "my ${NAME} ${name} ${NaMe}"
+
+#4 varaible name cannot start with with any number 
+
