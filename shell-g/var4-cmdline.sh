9b8b3e864f707a9a913fc6e4cba976d84c337fd8
Your Name <you@example.com>
2025-01-27 12:55:43 +0000

    var4

---
+++ shell-g/var4-cmdline.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#name=syedrameez
+#age=30
+
+echo my name is ${name} and age is ${age}
+
+
+echo $1
+echo $2
+echo $3
+echo $4
+echo $5
+echo $6
+echo $7
+echo $8
+echo $9
+echo $11
+echo $12
+echo $13
