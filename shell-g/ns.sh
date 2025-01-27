1c1cdda798e71e55b587f8bff2248c9b2af258e3
Your Name <you@example.com>
2025-01-27 07:32:06 +0000

    echo-gaurav-1

---
+++ shell-g/ns.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo -e "\e my name is syed rameez"
+echo 'my name is syeda mahira'
+#owner=gaurav
+#properties=syed
+
+
+echo " my \
+name \
+is \
+syed \
+rameez
+"
+
+echo -e "my name is \t syeda  \t mahira"
+echo -e "my name is \v syeda \v mahira"
+echo -e "my name us \n syed \n rameez"
+
+<<comment
+echo "becuase im syed"
+comment
