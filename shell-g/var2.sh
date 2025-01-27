368cd9f67d1bc44bdcc223975381c2e4ac66de2d
Your Name <you@example.com>
2025-01-27 08:25:39 +0000

    4

--- shell-g/var2.sh
+++ shell-g/var2.sh
@@ -19,3 +19,12 @@ echo "my ${NAME} ${name} ${NaMe}"
 
 #4 varaible name cannot start with with any number 
 
+
+####################################################################################################################################################################################################################################
+
+
+#syestem Defined varible put env and check on terminal 
+
+
+echo "my shell is ${SHELL} and present working directory is ${PWD} and logname is ${LOGNAME} and home is ${HOME} and path is ${PATH}"
+
