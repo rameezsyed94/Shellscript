6a1764691b4afe9c5e8caf3c2d224b4da77d5903
Your Name <you@example.com>
2025-02-19 12:34:16 +0000

    f4

--- shell-g/function/f3.sh
+++ shell-g/function/f3.sh
@@ -2,17 +2,21 @@
 
 function install(){          #first method statement function name(){content} and call
 #installtion code
+echo "executing ${FUNCNAME} - start"
 echo "installing ${2} ${1}"   # calling $2 from function call where we find inputs
+echo "executing ${FUNCNAME} - end"
 }
 
 config(){                  #second method just ignore the word function rest means the same
 #configuration code
 echo "config ${1} ${2}"
+echo "${FUNCNAME}"
 }
 
 function deploy {          # third method just ignore the round braces i.e()
 # deploy code
 echo "deploy code ${1}"
+echo "${FUNCNAME}"
 }
 
 #call the function in any order
