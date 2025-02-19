e711b44918b4e4d51244e27b424902b1a4354691
Your Name <you@example.com>
2025-02-19 12:22:51 +0000

    f3

---
+++ shell-g/function/f3.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+function install(){          #first method statement function name(){content} and call
+#installtion code
+echo "installing ${2}"   # calling $2 from function call where we find inputs
+}
+
+config(){                  #second method just ignore the word function rest means the same
+#configuration code
+echo "config ${1}"
+}
+
+function deploy {          # third method just ignore the round braces i.e()
+# deploy code
+echo "deploy code ${1}"
+}
+
+#call the function in any order
+install "nginx" "webserver" #ex $1 and $2 ....
+config "nginx-apache" "httpd"
+deploy "web-apps" "lms"
